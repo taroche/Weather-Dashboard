a9b41c8cf03108a47d3fe07634196ff709d2051f
Tristan LaRoche <laroche11235@gmail.com>
2021-01-27 18:09:35 -0500

    ajax call comment out

--- js/main.js
+++ js/main.js
@@ -3,4 +3,18 @@ $("window").ready(function () {
 
     $(".currentDate").text(now.format("(M/D/YYYY)"));
 })
+// const apiKey = "2da202c3060a41dedb520da5c37123a2";
+// const city = "Portland";
 
+
+//   $.ajax({
+//     url: `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`,
+//     method: "GET",
+//     error: function (err) {
+        
+//         errorModal(err);
+//     }
+// }).then(function (response) {
+//     console.log(response);
+  
+// });
