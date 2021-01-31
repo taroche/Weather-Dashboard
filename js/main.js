f8afdeb57fe92def065e2ae58ac614d4fe4c1014
Tristan LaRoche <laroche11235@gmail.com>
2021-01-31 16:40:00 -0500

    click search button make ajax call and new button

--- js/main.js
+++ js/main.js
@@ -1,83 +1,32 @@
 let now = dayjs();
 const $searchEl = $(".search");
-const $newCity = $(".btnNewCity");
 const $weatherEl = $(".weather");
 const $fiveDayEl = $(".5day");
 const $tempEl = $(".temp");
 const $currentCityEl = $(".currentCity")
-const $weatherIconEl = $(".weatherIcon")
+const $weatherIconEl = $("#wicon")
 const $humidityEL = $(".humidity")
 const $windSpeedEl = $(".windSpeed")
 const $uvEL = $(".UV")
+const $newCitySearchEl = $("#newCitySearch")
+const apiKey = "2da202c3060a41dedb520da5c37123a2";
+let cityArr = [];
 
-$("window").ready(function () {
-
-    $searchEl.on("click", function (e) {
-        e.preventDefault()
-        let value = $(this).prev().val()
-        //want to do this better
-        let btn = document.createElement("BUTTON");
-        btn.innerHTML = value;
-        btn.classList.add("btnNewCity");
-        console.log(btn)
-        document.getElementById('newCitySearch').appendChild(btn)
-        localStorage.setItem(value, value)
-    });
-
-    //     $newCity.on("click", function (e) {
-    //         e.preventDefault()
-    //         $(".currentDate").text(now.format("(M/D/YYYY)"));
-    //         //let value = $(this).val()
-    //         const apiKey = "2da202c3060a41dedb520da5c37123a2";
-    //         const value = "04008";
-
-
-    //         $.ajax({
-    //             url: `https://api.openweathermap.org/data/2.5/forecast?zip=${value},us&appid=${apiKey}`,
-    //             method: "GET",
-    //             error: function (err) {
-
-    //                 errorModal(err);
-    //             }
-    //         }).then(function (response) {
-    //             currentWeather(response);
-
-    //         });
-
-    //         function currentWeather(data) {
-    //             $currentCityEl.text(data.city.name)
-    //             $weatherIconEl.text(data.weather.icon)
-    //             $tempEl.text(data.main.temp)
-    //             $humidityEL.text(data.main.humidity)
-    //             windSpeedEl.text(data.wind.speed)
-
-
-    // })
-
-    const apiKey = "2da202c3060a41dedb520da5c37123a2";
-    const value = "04008";
-
-
+$searchEl.on("click", function (e) {
+    e.preventDefault()
+    let value = $(this).prev().val()
     $.ajax({
-        url: `https://api.openweathermap.org/data/2.5/forecast?zip=${value},us&appid=${apiKey}`,
+        url: `https://api.openweathermap.org/data/2.5/forecast?q=${value},us&appid=${apiKey}`,
         method: "GET",
         error: function (err) {
-
-            errorModal(err);
+            alert("not a city");
         }
-    }).then(function (response) {
-        currentWeather(response);
-
-    });
-
-    function currentWeather(data) {
-        $(".currentDate").text(now.format("(M/D/YYYY)"));
-        $currentCityEl.text(data.city.name)
-        $weatherIconEl.src =`http://openweathermap.org/img/wn/${data.list[0].weather.icon}.png`
-        $tempEl.text(Math.round(((parseFloat(data.list[0].main.temp)-273.15)*1.8)+32))
-        $humidityEL.text(data.list[0].main.humidity)
-        $windSpeedEl.text(data.list[0].wind.speed)
-    }
-
-})
-
+    }).then(res => {
+        let $btn = $(`<button></button>`)
+        $btn.attr("class", "btnNewCity btn btn-primary col-12 col-lg-5 offset-lg-1")
+        $btn.attr("type", "button")
+        $btn.text(`${res.city.name}`)
+        $btn.attr("value", `${res.city.name}`)
+        $newCitySearchEl.append($btn)
+    })
+});
